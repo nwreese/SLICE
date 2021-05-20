5be5ed1201d3c9eabee125ac8865a33f08b104a2
Nick Reese <nwreese@kent.edu>
2021-05-19 23:16:51 -0400

    contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,18 +8,44 @@ import imageOne from './tig4.png'
 import imageTwo from './tig6.png'
 import imageThree from './tig7.png'
 import imageFour from './tig.png'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { SocialIcon } from 'react-social-icons';
+import logo from './tig.png';
+import logo2 from './tig6.png';
 
- class Contact extends Component {
-   
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 0,
-    }
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 400,
+    margin: 'auto',
+    backgroundColor: 'rgba(52, 52, 52, 0.8)',
+    color: 'rgba(255, 99, 71, 1)'
+  },
+  media: {
+    height: 240,
+    marginTop: 200,
+  },
+  button: {
+    color: 'rgba(255, 99, 71, 1)',
+    margin: 'auto'
   }
-  onChange = e => this.setState({ value: e.target ? e.target.value : e });
+});
 
-  render() {
+ function Contact() {
+   
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     value: 0,
+  //   }
+  // }
+  // onChange = e => this.setState({ value: e.target ? e.target.value : e });
+
+  // return {
     
 // class Contact extends Component {
 //   constructor(props){
@@ -52,7 +78,7 @@ import imageFour from './tig.png'
 //     return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
 //   })
 // }
-
+const classes = useStyles();
 return (
   
   <div className="register-image" 
@@ -80,6 +106,51 @@ return (
         // value={this.state.value}
         // onChange={this.onChange}
       >
+        <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={logo}
+          title="Miso corn scallops"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Miso Corn Scallops
+          </Typography>Miso corn scallops, carrot bacon hash and beech mushroom
+          <Typography variant="body2" color="textSecondary" component="p">
+           
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        View on
+        <br></br>
+      <SocialIcon  bgColor="#F06000" style={{height: 30, width: 30}} url="https://www.instagram.com/p/COoCb02Hlv9/" />
+      </CardActions>
+    </Card>
+
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={logo2}
+          title="Watermellon Carpaccio"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Watermelon carpaccio
+          </Typography>Watermelon carpaccio/ bocconcini/ fennel/ mint/ basil/ coconut chips/ watermelon radish/ coconut vinaigrette
+          <Typography variant="body2" color="textSecondary" component="p">
+           
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        View on
+        <br></br>
+      <SocialIcon  bgColor="#F06000" style={{height: 30, width: 30}} url="https://www.https://www.instagram.com/p/COUcGYwHjVm/" />
+      </CardActions>
+    </Card>
          <img src={`${imageFour}`} />
         <img src={`${imageOne}`} />
         <img src={`${imageTwo}`} />
@@ -90,7 +161,7 @@ return (
   // </div>
 )
     }
-  } 
+  
   
            
        
